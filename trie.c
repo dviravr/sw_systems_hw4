f9fab5f93cdc1d93f66c2ce008ecedab3e542b2a
dviravr <dviravr@gmail.com>
2021-01-15 09:31:21 +0200

    print works!!

--- trie.c
+++ trie.c
@@ -42,7 +42,7 @@ void getWords(Node** head) {
         } else if (ch >= 'A' && ch <= 'Z') {
             ch += 'a' - 'A';
             node = insertChar(node, ch);
-        } else if (ch == ' ' || ch == '\t' || ch == EOF) {
+        } else if ((ch == ' ' || ch == '\t' || ch == '\n' || ch == EOF) && node != *head) {
             node->count++;
             node = *head;
         }
@@ -56,47 +56,16 @@ int haveChildren(Node* curr) {
             return TRUE;
     return FALSE;
 }
-/*
-void printTrie(Node** head, Node** curr){
-    Node* curr = *curr;
-    printf("%c", node->letter);
-    if(node->count != 0){
-        printf(" %d", node->count);
-        node->count = 0;
-        printTrie(head,head);
-    }
-    for(int i= 0; i<NUM_LETTERS; i++){
-        if(node->count != 0)
-        
-        if(node->children[i] != NULL){
-            printTrie()
-        }
-    }
 
-void printTrie(Node** head){
-        Node* curr = *head;
-        while( !haveChildren(*head)){
-            curr->
-        }
-
-}
-*/
-
-void freeNodes(Node** node) {
-    Node** temp = node;
-    Node* father = (*node)->father;
-    while (father != NULL) {
-        // printf("temp: %c %d\n", *(temp)->letter, *(temp)->count);
-        printf("father: %c %d\n", father->letter, father->count);
-        free(*temp);
-        *temp = NULL;
-        if (haveChildren(father)) {
-            puts("TRUE");
-        } else {
-            puts("FALSE");
-        }
-        temp = &father;
-        // temp = father;
+// free the node and all his fathers that dont hava children
+void freeNodes(Node* node) {
+    Node* father = node->father;
+    int index = -1;
+    while (father != NULL && !haveChildren(node)) {
+        index = node->letter - 'a';
+        free(node);
+        father->children[index] = NULL;
+        node = father;
         father = father->father;
     }
 }
@@ -106,14 +75,16 @@ void printSubtree(Node* head) {
     int i;
     while (head != NULL) {
         if (curr->count > 0) {
+            if (curr == head) 
+                puts("shouldn't be here");
             printf("%c %d\n", curr->letter, curr->count);
             curr->count = 0;
             if (!haveChildren(curr)) {
-                freeNodes(&curr);
+                freeNodes(curr);
             }
             curr = head;
         } else if (!haveChildren(curr)) {
-            freeNodes(&curr);
+            freeNodes(curr);
             curr = head;
         } else {
             for (i = 0; i < NUM_LETTERS; i++) {
@@ -126,117 +97,17 @@ void printSubtree(Node* head) {
                 }
             }
         }
-        /*
-            for (i = 0; i < NUM_LETTERS; i++)
-            {
-                if (curr->count>0)
-                {
-                    printf( "%c %d\n ",curr->letter, curr->count);
-                    curr-> count =0;
-                    curr = head;
-                    i=NUM_LETTERS;
-                }
-                else if (curr->children[i] != NULL)
-                {
-                    printf("%c", curr->letter);
-                    curr= curr->children[i];
-                    i=0;
-                }
-            }
+        if (!haveChildren(head)) {
+            break;
         }
-        else 
-        {
-            if (curr->count>0)
-            {
-                printf( "%c %d\n ",curr->letter, curr->count);
-                curr-> count =0;
-            }
-            free(curr);
-            curr=NULL;
-            curr = head;
-        }*/
     }
 }
-/*
-void printSubtree1(Node* head ,Node* curr, int index) {
 
-    if (curr == NULL){
-        return;
-        }
-
-    if (curr->count != 0) {     
-        printf( "%c %d\n", curr->letter ,curr->count);
-        curr-> count =0;
-        printSubtree1(head,head,0);
-    }
-     
-    if(!haveChildren(curr))
-    {
-        curr 
-    }
-
-    printf("%c", curr->letter);
-
-    if (curr -> count != 0) {     
-        printf(" %d\n", curr->count);
-        curr = head;
-        }
-
-    for (int i = 0; i<26;i++) {    
-        if (curr->children[i]!= NULL) {   
-            printSubtree1( head , curr->children[i], index +1);
-            curr->children[i]= NULL;
-           // free(curr->children[i]);
-        }           
-    }
-
-               
- }
-*/
-// }
-
-// void printTrie(Node *node) {
-//     // Node* node = head;
-//     while (node != NULL) {
-//         printf("%c, %lu\n", node->letter, node->count);
-//         for (size_t i = 0; i < NUM_LETTERS; i++)
-//         {
-//             printTrie(node->children[0]);
-//         }
-//     }
-// }
 int main() {
     Node* head = newNode('\0', NULL);
-
     getWords(&head);
-    head->children[0]->count = 1;
-    head->children[0]->children[1]->children[2]->count = 0;
-    Node** n = &(head->children[0]->children[1]->children[2]);
-    freeNodes(n);
-    if (head->children[0] != NULL) {
-        puts("shouldn't be null");
-    }
-    // // head->children[0]->children[1]->children[2] = NULL;
-    // *n = NULL;
-    if (head->children[0]->children[1] == NULL) {
-        puts("should be null 1");
-    }
-
-    if (head->children[0]->children[1]->children[2] == NULL) {
-        puts("should be null 2");
-    }
-
-    // putc(n->letter, stdout);
-    // while (n != NULL) {
-    //     putc(n->letter, stdout);
-    //     n = n->father;
-    // }
-
-    // printSubtree(head);
-    // printf("aa: %u\n", head->children[0]->children[0]->count);
-    // printf("aaa: %u\n", head->children[0]->children[0]->children[0]->count);
-    // printf("aba: %u\n", head->children[0]->children[1]->children[0]->count);
-    // insertChar(head, 'g');
-    // printf("%c\n", head->children['g' - 'a']->letter);
+    printSubtree(head);
+    free(head);
+    head = NULL;
     return 0;
 }
